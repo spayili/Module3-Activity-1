a405e9f760e813739c372af15ede50b69da5fa1f
spayili <sharath.ln253@gmail.com>
2024-03-19 01:02:04 -0700

    Initial commit
    Changes to be committed:
    	new file:   .env
    	new file:   .gitignore
    	new file:   index.js
    	new file:   package-lock.json
    	new file:   package.json

---
+++ index.js
@@ -0,0 +1,30 @@
+// Student Name: Sharath Payili
+// Student ID: 1225905683
+// Date: 02/18/2024
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PWD}@${MONGODB_CLSTR}/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
